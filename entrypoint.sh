16d5c5707442268258d4855b84f12711fd8229a1
Emil <33729521+Ollebolle22@users.noreply.github.com>
2025-06-30 11:30:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 set -e
-
-# Starta ett virtuellt X och kör GUI:t i samma display
-exec xvfb-run --auto-servernum --server-num=1 \
-     "$APP_HOME/gui-linux" "$@"
+exec xvfb-run --auto-servernum --server-num=1 "$APP_HOME/gui-linux"
