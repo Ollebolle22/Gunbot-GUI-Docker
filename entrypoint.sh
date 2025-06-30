381b8511a9288d4134a172332a70b0978abdb695
Emil <33729521+Ollebolle22@users.noreply.github.com>
2025-06-30 11:36:18 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 set -e
-exec xvfb-run --auto-servernum --server-num=1 "$APP_HOME/gui-linux"
+# Kör GUI:t i ett headless X-fönster
+exec xvfb-run --auto-servernum --server-num=1 "$APP_HOME/gui-linux" "$@"
